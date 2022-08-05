c5d0973d2880a78b0cbf07f18c849b7c0d69f534
Duonghieu97st <Duonghieu97st@gmail.com>
2022-08-05 10:47:43 +0700

    update main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Các số nguyên tố nhỏ hơn 100 là: ");
+        for (int i = 0; i < 100; i++) {
+            if (isPremeNumber(i)) {
+                System.out.print(i + " ");
+            }
+        }
+
+    }
+
+    public static boolean isPremeNumber(int n) {
+        if (n < 2) {
+            return false;
+        }
+        // check so nguyen to khi n >= 2
+        int squareRoot = (int) Math.sqrt(n);
+        for (int i = 2; i <= squareRoot; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
